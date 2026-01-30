031c3b4c08366827fb82743eb13dace2fa49a217
TheGreenCoconut <quinn.harris@icloud.com>
2026-01-29 21:49:21 -0800

    Execute the Orphans

--- Unit-4/CookieMonster/CookieMonster.java
+++ Unit-4/CookieMonster/CookieMonster.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-// You are allowed (and expected!) to use either Java's ArrayDeque or LinkedList class to make stacks and queues
+// You are allowed (and expected!) to use either Java's ArrayDeque or LinkedList class to make
+// stacks and queues
 
 public class CookieMonster {
 
@@ -35,6 +36,9 @@ public class CookieMonster {
 	}
 
 	public CookieMonster(int[][] cookieGrid) {
+		if (cookieGrid == null) {
+			throw new IllegalArgumentException();
+		}
 		this.cookieGrid = cookieGrid;
 		this.numRows = cookieGrid.length;
 		this.numCols = cookieGrid[0].length;
@@ -42,41 +46,46 @@ public class CookieMonster {
 
 	// You may find it VERY helpful to write this helper method. Or not!
 	private boolean validPoint(int row, int col) {
-		return !((row > cookieGrid.length - 1 || col > cookieGrid[row].length - 1) || (cookieGrid[row][col] == -1));
+		return !((row > cookieGrid.length - 1 || col > cookieGrid[row].length - 1)
+				|| (cookieGrid[row][col] == -1));
 	}
 
 	/*
-	 * RECURSIVELY calculates the route which grants the most cookies.
-	 * Returns the maximum number of cookies attainable.
+	 * RECURSIVELY calculates the route which grants the most cookies. Returns the maximum number of
+	 * cookies attainable.
 	 */
 	public int recursiveCookies() {
-		return recursiveCookies(0, 0);
+		int result = recursiveCookies(0, 0);
+		return result == -1 ? 0 : result;
 	}
 
 	// Returns the maximum number of cookies edible starting from (and including)
-	// cookieGrid[row][col]
+	// cookieGrid[row][col], or -1 if no path to bottom-right exists
 	public int recursiveCookies(int row, int col) {
 		if (!validPoint(row, col) || cookieGrid[row][col] == -1) {
-			return 0;
-		} else {
-			int sum = cookieGrid[row][col], goRight = recursiveCookies(row, col + 1),
-					goLeft = recursiveCookies(row + 1, col);
-			sum += goRight > goLeft ? goRight : goLeft;
-			return sum;
+			return -1;
 		}
+		if (row == cookieGrid.length - 1 && col == cookieGrid[0].length - 1) {
+			return cookieGrid[row][col];
+		}
+		int goRight = recursiveCookies(row, col + 1), goDown = recursiveCookies(row + 1, col);
+		if (goRight == -1 && goDown == -1) {
+			return -1;
+		}
+		return cookieGrid[row][col] + Math.max(goRight, goDown);
 	}
 
 	/*
-	 * Calculate which route grants the most cookies using a QUEUE.
-	 * Returns the maximum number of cookies attainable.
+	 * Calculate which route grants the most cookies using a QUEUE. Returns the maximum number of
+	 * cookies attainable.
 	 */
 	/*
-	 * From any given position, always add the path right before adding the path
-	 * down
+	 * From any given position, always add the path right before adding the path down
 	 */
 	public int queueCookies() {
 		ArrayDeque<OrphanScout> queue = new ArrayDeque<OrphanScout>();
 		queue.add(new OrphanScout(0, 0, cookieGrid[0][0]));
+		int max = 0;
 		boolean cease = false;
 		while (!cease) {
 			cease = true;
@@ -86,16 +95,12 @@ public class CookieMonster {
 				int currentRow = curr.getEndingRow(), currentCol = curr.getEndingCol(),
 						numCookies = curr.getCookiesDiscovered();
 				if (validPoint(currentRow, currentCol + 1)) {
-					queue.add(new OrphanScout(
-							currentRow,
-							currentCol + 1,
+					queue.add(new OrphanScout(currentRow, currentCol + 1,
 							numCookies + cookieGrid[currentRow][currentCol + 1]));
 				}
 
 				if (validPoint(currentRow + 1, currentCol)) {
-					queue.add(new OrphanScout(
-							currentRow + 1,
-							currentCol,
+					queue.add(new OrphanScout(currentRow + 1, currentCol,
 							numCookies + cookieGrid[currentRow + 1][currentCol]));
 				}
 				queue.remove();
@@ -103,37 +108,47 @@ public class CookieMonster {
 			length = queue.size();
 			for (int i = 0; i < length; i++) {
 				OrphanScout curr = queue.remove();
-				if (curr.getEndingRow() != numRows - 1 ||
-						curr.getEndingCol() != numCols - 1) {
+				if (curr.getEndingRow() != numRows - 1 || curr.getEndingCol() != numCols - 1) {
 					cease = false;
 				}
 				queue.add(curr);
 			}
 		}
-		int max = 0;
 		while (!queue.isEmpty()) {
-			int cookies = queue.remove().getCookiesDiscovered();
-			if (cookies > max) {
-				max = cookies;
-			}
+			max = Math.max(max, queue.remove().getCookiesDiscovered());
 		}
 
 		return max;
 	}
 
 	/*
-	 * Calculate which route grants the most cookies using a stack.
-	 * Returns the maximum number of cookies attainable.
+	 * Calculate which route grants the most cookies using a stack. Returns the maximum number of
+	 * cookies attainable.
 	 */
 	/*
-	 * From any given position, always add the path right before adding the path
-	 * down
+	 * From any given position, always add the path right before adding the path down
 	 */
 	public int stackCookies() {
-		Stack<OrphanScout> stack = new Stack<OrphanScout>();
-		stack.add(new OrphanScout(0, 0, cookieGrid[0][0]));
+		ArrayDeque<OrphanScout> stack = new ArrayDeque<>();
+		stack.push(new OrphanScout(0, 0, cookieGrid[0][0]));
 		int max = 0;
-
+		while (!stack.isEmpty()) {
+			OrphanScout curr = stack.pop();
+			int currentRow = curr.getEndingRow(), currentCol = curr.getEndingCol(),
+					numCookies = curr.getCookiesDiscovered();
+			if (validPoint(currentRow + 1, currentCol)) {
+				stack.push(new OrphanScout(currentRow + 1, currentCol,
+						numCookies + cookieGrid[currentRow + 1][currentCol]));
+			}
+			if (validPoint(currentRow, currentCol + 1)) {
+				stack.push(new OrphanScout(currentRow, currentCol + 1,
+						numCookies + cookieGrid[currentRow][currentCol + 1]));
+			}
+			if (currentRow == numRows - 1 && currentCol == numCols - 1) {
+				max = Math.max(max, numCookies);
+				continue;
+			}
+		}
 		return max;
 	}
 }
